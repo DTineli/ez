03907da7c4cff497a3e9ca840a8ac1c92d791f1b
Daniel Tineli Jr <danieltineli.td@gmail.com>
2026-02-09 17:33:06 -0300

    adicionei o dbstore do user

--- internal/store/dbstore/user.go
+++ internal/store/dbstore/user.go
@@ -1,1 +1,25 @@
 package dbstore
+
+import (
+	"github.com/DTineli/ez/internal/store"
+	"gorm.io/gorm"
+)
+
+type UserStore struct {
+	db *gorm.DB
+}
+
+func NewUserStore(db *gorm.DB) *UserStore {
+	return &UserStore{
+		db: db,
+	}
+}
+
+func (u *UserStore) CreateUser(user store.UserDTO) (int, error) {
+	return 0, nil
+}
+
+func (u *UserStore) GetUser(email string) (*store.User, error) {
+
+	return nil, nil
+}
