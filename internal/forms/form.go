8e8448e7afeccaade4ee656ccf3c09385e006962
Daniel Tineli Jr <danieltineli.td@gmail.com>
2026-02-23 08:11:28 -0300

    criei o pacote forms

---
+++ internal/forms/form.go
@@ -0,0 +1,74 @@
+package forms
+
+import (
+	"fmt"
+	"net/url"
+	"strconv"
+	"strings"
+	"unicode/utf8"
+)
+
+type Form struct {
+	url.Values
+	Errors errors
+}
+
+func New(data url.Values) *Form {
+	return &Form{
+		data,
+		errors(map[string][]string{}),
+	}
+}
+
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		if strings.TrimSpace(f.Get(field)) == "" {
+			f.Errors[field] = append(f.Errors[field], "Esse valor não pode ser vazio.")
+		}
+	}
+}
+
+func (f *Form) MaxLength(field string, length int) {
+	if utf8.RuneCountInString(f.Get(field)) > length {
+		f.Errors[field] = append(f.Errors[field], fmt.Sprintf("%v deve ter no maximo %v caracteres", field, length))
+	}
+}
+
+func (f *Form) MinLength(field string, length int) {
+	value := f.Get(field)
+	if value == "" {
+		return
+	}
+
+	if utf8.RuneCountInString(value) < length {
+		f.Errors[field] = append(f.Errors[field], fmt.Sprintf("%v deve ter no minimo %v caracteres", field, length))
+	}
+}
+
+func (f *Form) IsFloat(field string) {
+	value := f.Get(field)
+
+	if value == "" {
+		return
+	}
+
+	if _, err := strconv.ParseFloat(value, 64); err != nil {
+		f.Errors[field] = append(f.Errors[field], "Deve ser um número válido")
+	}
+}
+
+func (f *Form) IsInt(field string) {
+	value := f.Get(field)
+
+	if value == "" {
+		return
+	}
+
+	if _, err := strconv.Atoi(value); err != nil {
+		f.Errors[field] = append(f.Errors[field], "Deve ser um número inteiro válido")
+	}
+}
+
+func (f *Form) Valid() bool {
+	return len(f.Errors) == 0
+}
