3f38112794e95a04bcf292c6ca30ef4f60af61e7
Daniel Tineli Jr <danieltineli.td@gmail.com>
2026-03-11 11:56:02 -0300

    btn de gerar link aparecendo na tela

--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/cli/browser v1.3.0 // indirect
 	github.com/fatih/color v1.16.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/securecookie v1.1.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
---
+++ internal/store/inviteStore.go
@@ -0,0 +1,29 @@
+package store
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Invite struct {
+	ID       uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
+	Document string    `gorm:"not null;unique"`
+	Phone    string
+
+	OriginTenant uint
+	Tenant       Tenant `gorm:"foreignKey:OriginTenant"`
+
+	UserID *uint
+	User   *User `gorm:"foreignKey:UserID"`
+}
+
+func (i *Invite) BeforeCreate(tx *gorm.DB) error {
+	i.ID = uuid.New()
+	return nil
+}
+
+type InviteStore interface {
+	Find(filters any) ([]Invite, error)
+	FindByID(id uuid.UUID) (Invite, error)
+	Create(*Invite) error
+}
